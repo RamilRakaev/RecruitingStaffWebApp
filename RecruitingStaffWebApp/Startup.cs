ed84c12d4d38e10e32350dff37bfc1b503e9b6b3
Рамиль Ракаев <CHICO5Listo4@gmail.com>
2021-11-29 17:33:18 +0300

    Добавлен метод расширения ConfigureService для стартапа

--- RecruitingStaffWebApp/Startup.cs
+++ RecruitingStaffWebApp/Startup.cs
@@ -1,25 +1,14 @@
-using RecruitingStaff.Infrastructure.Repositories;
+using FluentValidation.AspNetCore;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
-using FluentValidation;
-using FluentValidation.AspNetCore;
-using MediatR;
-using RecruitingStaff.Infrastructure.CQRS;
-using RecruitingStaff.Infrastructure.DatabaseServices;
-using RecruitingStaff.Domain.Model.UserIdentity;
-using RecruitingStaff.Domain.Model.CandidateQuestionnaire;
-using RecruitingStaff.Domain.Interfaces;
 using RecruitingStaff.Domain.Model;
-using RecruitingStaff.Domain.Validators;
-using RecruitingStaffWebApp.Services.DocParse;
-using RecruitingStaffWebApp.Infrastructure.DocParse;
-using RecruitingStaff.Domain.Model.CandidateQuestionnaire.CandidateData;
-using Microsoft.Extensions.Logging;
-using Microsoft.AspNetCore.Http;
+using RecruitingStaff.Domain.Model.UserIdentity;
+using RecruitingStaff.Infrastructure.Repositories;
+using RecruitingStaff.WebApp;
 
 namespace RecruitingStaffWebApp
 {
@@ -39,23 +28,9 @@ namespace RecruitingStaffWebApp
                 o => o.MigrationsAssembly(typeof(DataContext).Assembly.FullName)));
             services.AddIdentity<ApplicationUser, ApplicationRole>().AddEntityFrameworkStores<DataContext>();
 
-            services.AddTransient(typeof(IRepository<>), typeof(BaseRepository<>));
-
-            services.AddHostedService<MigrationService>();
-            services.AddHostedService<UserService>();
-
             services.Configure<WebAppOptions>(Configuration.GetSection("WebAppOptions"));
 
-            services.AddTransient<IQuestionnaireManager, QuestionnaireManager>();
-
-            services.AddMediatR(CQRSAssemblyInfo.Assembly);
-            services.AddTransient(typeof(IPipelineBehavior<,>), typeof(ValidationBehaviour<,>));
-            services.AddValidatorsFromAssembly(CQRSAssemblyInfo.Assembly);
-
-            services.AddTransient<IValidator<ApplicationUser>, ApplicationUserValidator>();
-            services.AddTransient<IValidator<Candidate>, CandidateValidator>();
-            services.AddTransient<IValidator<Vacancy>, VacancyValidator>();
-            services.AddTransient<IValidator<Questionnaire>, QuestionnaireValidator>();
+            services.ConfigureService();
 
             services.AddRazorPages().AddFluentValidation();
             services.AddAuthorization(option =>
---
+++ RecruitingStaffWebApp/StartupExtensions.cs
@@ -0,0 +1,38 @@
+﻿using FluentValidation;
+using MediatR;
+using Microsoft.Extensions.DependencyInjection;
+using RecruitingStaff.Domain.Interfaces;
+using RecruitingStaff.Domain.Model.CandidateQuestionnaire;
+using RecruitingStaff.Domain.Model.CandidateQuestionnaire.CandidateData;
+using RecruitingStaff.Domain.Model.UserIdentity;
+using RecruitingStaff.Domain.Validators;
+using RecruitingStaff.Infrastructure.CQRS;
+using RecruitingStaff.Infrastructure.DatabaseServices;
+using RecruitingStaff.Infrastructure.Repositories;
+using RecruitingStaffWebApp.Infrastructure.DocParse;
+using RecruitingStaffWebApp.Services.DocParse;
+
+namespace RecruitingStaff.WebApp
+{
+    public static class StartupExtensions
+    {
+        public static void ConfigureService(this IServiceCollection services)
+        {
+            services.AddTransient(typeof(IRepository<>), typeof(BaseRepository<>));
+
+            services.AddHostedService<MigrationService>();
+            services.AddHostedService<UserService>();
+
+            services.AddTransient<IQuestionnaireManager, QuestionnaireManager>();
+
+            services.AddMediatR(CQRSAssemblyInfo.Assembly);
+            services.AddTransient(typeof(IPipelineBehavior<,>), typeof(ValidationBehaviour<,>));
+            services.AddValidatorsFromAssembly(CQRSAssemblyInfo.Assembly);
+
+            services.AddTransient<IValidator<ApplicationUser>, ApplicationUserValidator>();
+            services.AddTransient<IValidator<Candidate>, CandidateValidator>();
+            services.AddTransient<IValidator<Vacancy>, VacancyValidator>();
+            services.AddTransient<IValidator<Questionnaire>, QuestionnaireValidator>();
+        }
+    }
+}
